f9941a3cddca5dfb4530c2efa6db4ce46f8ed50e
Nishant Roy <nroy@gatech.edu>
2017-10-11 01:05:23 -0400

    Comments

--- main.go
+++ main.go
@@ -8,17 +8,20 @@ import (
 
 
 func main() {
+
+	// User Handlers
 	http.HandleFunc("/api/v1/users/weekly_plan", handleGetWeeklyPlan)
 	http.HandleFunc("/api/v1/users/shopping_list", handleGetShoppingList)
+
+	// Recipe Handlers
 	http.HandleFunc("/api/v1/recipes/recipe_steps", handleGetRecipeSteps)
 	http.HandleFunc("/api/v1/recipes/recipe_details", handleGetRecipeDetails)
+
+	// Default
 	http.HandleFunc("/", handler)
 
-
+	// Start routing
 	err := http.ListenAndServe(":8080", nil)
-	fmt.Println(err.Error())
-}
 
-func init() {
-	main()
+	fmt.Println(err.Error())
 }
